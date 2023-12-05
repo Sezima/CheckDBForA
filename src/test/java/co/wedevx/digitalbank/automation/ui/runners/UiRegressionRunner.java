e81f12c55c16ac12efcfe84a85232436625aee20
sezim <osmsezimka@gmail.com>
2023-12-06 05:25:39 +0600

    3

--- src/test/java/co/wedevx/digitalbank/automation/ui/runners/UiRegressionRunner.java
+++ src/test/java/co/wedevx/digitalbank/automation/ui/runners/UiRegressionRunner.java
@@ -1,5 +1,7 @@
 package co.wedevx.digitalbank.automation.ui.runners;
 
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
@@ -9,5 +11,9 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("ui/features")
 @ConfigurationParameter(key=GLUE_PROPERTY_NAME, value = "co.wedevx.digitalbank.automation.ui.steps")
 @IncludeTags("Test")
+@Cucumber
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json"}
+)
 public class UiRegressionRunner {
 }
